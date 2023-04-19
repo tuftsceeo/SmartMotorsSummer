f7700f51039f3f517b4ac2cefbe3b9566c00615f
billc2013 <williamchurch17@gmail.com>
2023-04-19 12:40:27 -0400

    Put old code in old folder; added comments; added animated dot selector

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 # COM speed 115200
 # with SCL in D1 and SDA in D2
-
-# SSD1306 GUIDE
-#display.contrast(255)
-#display.text('Hello, World!', 0, 0, 1)
-#display.pixel(0, 0, 1)
-#display.pixel(127, 63, 1)
-
-#display.show()
-#display.fill(0)
-#display.show()
-#display.hline(0, 8, 4, 1) # not working
-#display.rect(10, 10, 107, 43, 1) # not working
+#
+###############
+# Smart Motors Main.py code v7, April 19, 2023, bchurch edits
+###############
 
 from machine import Pin, SoftI2C, PWM, ADC
 import time
@@ -19,83 +11,58 @@ import smarttools
 import servo
 import smartfunctions
 
-
-
-
-
-
+# create our dipslay object from smarttools module
 i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
 display = smarttools.SSD1306_SMART(128, 64, i2c)
 
-
+# create our button user interface objects from smarttools module
 bup = smarttools.BUTTON(8)
 bselect = smarttools.BUTTON(9)
 bdown = smarttools.BUTTON(10)
 
-
-
-
+# create our servo, potentiometer, and light sensor objects
 s = servo.Servo(Pin(2))
-
-# pot pin GPIO3, A1, D1
-
 pot = ADC(Pin(3))
 pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
-# pot.read() returns integers in [0, 4095]
-
-
-# light pin GPIO5
 light = ADC(Pin(5))
 light.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
 
-# plot ranges from 4,4 to 78, 59 for the box not to overlap with the border
+# Create a range mapping function because we seek to plot values of different ranges (poteniometer of 0 to 4095) on the same scale as a range of the motor (0 to 180) and on a different scale for the screen (screenx and screeny)
 ranges = {'pot': [0, 4095], 'light': [0, 4095], 'motor': [0, 180], 'screenx': [4, 123], 'screeny': [59, 20], 'oldscreenx': [4, 78], 'oldscreeny': [59, 4]} # screeny is backwards because it is from top to bottom
 def transform(initial, final, value):
     initial = ranges[initial]
     final = ranges[final]
     return int((final[1]-final[0]) / (initial[1]-initial[0]) * (value - initial[0]) + final[0])
 
-
-
-
-
-
+# Define variables used in the main while loop
 mode = 0
 point = [9,9]
 points = []
+iterator = 0 #<---- for selector animation in smarttools module
 
-
-
-#display.text('setup', 4, 4, 1)  # its rectangle is ((2, 2), (45, 13))
-#display.text('train', 47, 4, 1) # its rectangle is ((45, 2), (88, 13))
-#display.text('test', 90, 4, 1)  # its rectangle is ((88, 2), (123 13))
-#display.show()
-
-
+# Main  while loop
 while(True):
+    # check to see if we have any button presses
     bdown.update()
     bselect.update()
     bup.update()
+
+    # act on button presses to change the mode from 0 (settings) to 1 (training) to 2 (play/test training model)
     if bdown.tapped or bselect.held:
         mode = (mode + 1) % 3
-    if mode == 1:
-        point = [transform('light', 'oldscreenx', light.read()), transform('pot', 'oldscreeny', pot.read())]
-        s.write_angle(transform('pot', 'motor', pot.read()))
+    if mode == 1:  #<------ training
+        point = [transform('light', 'oldscreenx', light.read()), transform('pot', 'oldscreeny', pot.read())]  #<---- define current point based on measured values of the potentiometer and light sensor
+        s.write_angle(transform('pot', 'motor', pot.read()))  #<----- move motor to position cooresponding to the potentiometer
         if bselect.tapped:
-            points.append(list(point))
-    elif mode == 2:
-        point = [transform('light', 'oldscreenx', light.read()), -20] 
-        motor_position =  smartfunctions.nearestNeighbor(points, point) #<---added wchurch april 12, 2023
-        # s.write_angle(transform('oldscreeny', 'motor', smartfunctions.nearestNeighbor(points, point)))
-        s.write_angle(transform('oldscreeny', 'motor', motor_position)) #<---modified wchurch april 12, 2023
-        point = [transform('light', 'oldscreenx', light.read()), motor_position]  #<----to show box during testing; added wchurch april 12, 2023
-
-
+            points.append(list(point))   #<------ add data point to training list
+    elif mode == 2:  #<------- play/test training model
+        point = [transform('light', 'oldscreenx', light.read()), -20]   #<----- define current point
+        motor_position =  smartfunctions.nearestNeighbor(points, point)  #<----- get cooresponding motor via nearest neighbor algorithm
+        s.write_angle(transform('oldscreeny', 'motor', motor_position))   #<----- move motor accordingly
+        point = [transform('light', 'oldscreenx', light.read()), motor_position]  #<---- define a new point to show a live data box around on the graph
     elif mode == 0:
-        pass
-        #point = [transform('light', 'screenx', light.read()), -20]
-        #s.write_angle(transform('screeny', 'motor', smartfunctions.extremeLine(points, point)))
-        #print(transform('screeny', 'motor', smartfunctions.extremeline(points, point)))
-    display.writeall(point, points, mode = mode)
+        pass   # fucntion for settings mode not defined yet
+    display.writeall(point, points, mode, iterator)  #<----- display everything
+    iterator += 1
 
 
