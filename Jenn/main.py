3679192278cc0eab1c128976dd3dc450f47c16b9
Jennifer Cross <jenncross@users.noreply.github.com>
2023-04-25 15:16:45 -0400

    jenn syncing with tanushree

--- Jenn/main.py
+++ Jenn/main.py
@@ -1,74 +1,223 @@
-# COM speed 115200
-# with SCL in D1 and SDA in D2
-
-# SSD1306 GUIDE
-#display.contrast(255)
-#display.text('Hello, World!', 0, 0, 1)
-#display.pixel(0, 0, 1)
-#display.pixel(127, 63, 1)
-
-#display.show()
-#display.fill(0)
-#display.show()
-#display.hline(0, 8, 4, 1) # not working
-#display.rect(10, 10, 107, 43, 1) # not working
-
-from machine import Pin, SoftI2C, PWM, ADC
+import gc
+gc.collect()
 import time
-import smarttools
 import servo
+
+import ujson
+import network
+import smarttools
+
+import usocket as socket
+import random as r
+import re
+
+import ubinascii
+import ujson
+
+from math import log
+from machine import Pin, SoftI2C, PWM, ADC
+
+from web import web_page
 
 i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
 display = smarttools.SSD1306_SMART(128, 64, i2c)
 
+# connect to wifi 
+wlan=network.WLAN(network.AP_IF)
+wlan.active(False)
+wlan.active(True)
 
-bleft = smarttools.BUTTON(9) # SELECT
-bcenter = smarttools.BUTTON(10) #DOWN
-bright = smarttools.BUTTON(8) #UP
+mac = ubinascii.hexlify(network.WLAN().config("mac")).decode()
+lastmac=bytearray(mac[-6:])
+lastmac[5]=lastmac[5]+1
+SSID= 'ESP_'+lastmac.decode().upper()
 
-s = servo.Servo(Pin(2))
+display.text(SSID, 20,20,1)
+display.show()
+print("Waiting for connection...")
+while (not wlan.isconnected()):
+    time.sleep(1)
+    
+print("Wifi has been connected...")
+print("Connect to ip address 192.168.4.1")
+display.text(SSID, 20,20,1)
+display.text("Now connect to", 10,30,1)
+display.text("192.168.4.1", 20,50,1)
+display.show()
 
-# pot pin GPIO3, A1, D1
-pot = ADC(Pin(3))
-pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
-# pot.read() returns integers in [0, 4095]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 80))
+s.listen(5)
+
+# CODE below is for Web Server
+# connect servo
+motor = servo.Servo(Pin(2))
+motor.write_angle(90)
 
 # light pin GPIO5
-light = ADC(Pin(5))
-light.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
+Sensor = ADC(Pin(5))
+Sensor.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
 
-# plot ranges from 4,4 to 78, 59 for the box not to overlap with the border
-ranges = {'pot': [0, 4095], 'light': [0, 4095], 'motor': [0, 180], 'screenx': [4, 78], 'screeny': [59, 4]} # screeny is backwards because it is from top to bottom
-def transform(initial, final, value):
-    initial = ranges[initial]
-    final = ranges[final]
-    return int((final[1]-final[0]) / (initial[1]-initial[0]) * (value - initial[0]) + final[0])
+global_sensor_data = 0
+global_motor_data = 0
 
-mode = 2
-point = [9,9]
-points = []
+# this is the dynamic training data 
+training_data = []
+# this is the training data that is read from the data file 
+training_data_from_file = []
+datafilename = "trainData.txt"
 
+STATE = 1
 
-import wifisettings
+def updateStateTEST():
+    global STATE
+    if (STATE != 0):
+        STATE = 0
 
+def updateStateTRAIN():
+    global STATE
+    if (STATE != 1):
+        STATE = 1
+    
+def read_sensor():
+    sens = str(Sensor.read())
+    return sens
 
+def updateSensor(data):
+    global global_sensor_data
+    global_sensor_data = data
 
+def updateMotor(data):
+    global global_motor_data
+    global_motor_data = data
+    print("global motor data changed to...")
+    print(global_motor_data)
 
-#buttoncenter = button(10, holdthreshold = 10)
+# saves training_data to a file trainData.txt
+# each line is motorData,lightSensor
+def saveDataToFile():
+    f=open("trainData.txt","w")
+    length = len(training_data)
+    for ind, val in enumerate(training_data):
+        lightSensor = val[0]
+        motor = val[1]
+        f.write(str(motor))
+        f.write(",")
+        f.write(str(lightSensor))
+        f.write('\n')
+    f.close()
 
+def removeData():
+    global training_data
+    training_data.pop()
+    saveDataToFile()
 
+# adds (motor, light) pair to training data 
+def addData(motor, light):
+    tup = (int(motor), int(light))
+    global training_data
+    training_data.append(tup)
+    saveDataToFile()
+  
+# updates training_data_from_file
+def updateDataReadFromFile(data_list):
+    global training_data_from_file
+    training_data_from_file = data_list
+  
+def runData():
+    sens = Sensor.read()
+    sens = int((100 * int(t))/4095)
+    light_val = []
+    motor_val = []
+    for (light, mot) in training_data_from_file:
+        dist = abs(sens - light)
+        light_val.append(dist)
+        motor_val.append(mot)
+    # get the index of the least light_val
+    index = light_val.index(min(light_val))
+    pos = motor_val[index]
+    print("SENSOR VALUE IS...")
+    print(sens)
+    print("MOTOR VALUE ROTATE TO IS...")
+    print(pos)
+    motor.write_angle(pos)
+    
+# reads the file where each line is the motor,light data
+# returns [(motor,light), (motor,light)]
+def readFileAndStoreData(filename):
+    with open("trainData.txt", "r") as values:
+        lines = values.readlines()
+    data_tuples = []
+    for l in lines:
+        as_list = l.split(",")
+        motor= as_list[0]
+        light = as_list[1]
+        tuples = (int(motor), int(light))
+        data_tuples.append(tuples)
+        updateDataReadFromFile(data_tuples)
+    
+while True:
+    try:
+        conn, addr = s.accept()
+        reply = web_page()
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        request = str(request)
+            
+        print('Content = %s' % request)
+            
+        # setting the sensor reading     
+        if request.find('/getDHT') == 6:
+            t = read_sensor()
+            x = int((100 * int(t))/4095)
+            updateSensor(x)
+            # set sensor variable
+            reply = str(x) + "|"
+        
+        if(STATE == 0):
+            runData()
+        
+        if request.find('/slider') == 6:
+            splitval = request.split("=", 1)
+            number = splitval[1].split(" ", 1)
+            value = number[0]
+            motor.write_angle(180-int(value))
+            # set motor variable 
+            updateMotor(180-int(value))
+                
+        if request.find('/?addvalue') == 6:
+            # write the data to the file
+            t = read_sensor()
+            x = int((100 * int(t))/4095)
+            updateSensor(x)
+            addData(global_motor_data, global_sensor_data)
+                
+        # removes the last data from the list    
+        if request.find('/?deletevalue') == 6:
+            removeData()
+        
+        if request.find('/?test') == 6:
+            print("TEST clicked")
+            # read the data from the file
+            updateStateTEST()
+            readFileAndStoreData(datafilename)
+        
+        if request.find('/?stop') == 6:
+            print("STOP clicked")
+            # read the data from the file
+            updateStateTRAIN()
+            
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(reply)
+        conn.close()
+    except Exception as e:
+        print(e)
+        break
+gc.collect()
+display.text("Error encountered", 20,20,1)
+display.show()
+s.close()
 
-
-while(True):
-    bleft.update()
-    bcenter.update()
-    bright.update()
-
-    point = [transform('light', 'screenx', pot.read()), transform('pot', 'screeny', light.read())]
-    s.write_angle(transform('pot', 'motor', pot.read()))
-    if bcenter.tapped:
-        points.append(list(point))
-    if bcenter.held:
-        points.append([point[0] - 15, point[1] - 15])
-
-    display.writeall(point, points, mode = mode)
---
+++ Jenn/web.py
@@ -0,0 +1,403 @@
+def web_page():
+    webpage = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Web Server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/b6fe3b813e.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    
+    <style>
+    h2 {
+        font-size: 2.3rem;
+        text-align: center;
+    }
+    h3 {
+        font-size: 2.3rem;
+        text-align: center;
+    }
+    p {
+        font-size: 1.9rem;
+        text-align: center;
+    }
+    
+    /* Progress bar code */
+    .progress {
+      position: relative;
+      width: 250px;
+      height: 40px;
+      background: #FFF47C;
+      border-radius: 5px;
+      overflow: hidden;
+      margin: auto;
+    }
+    
+    /* Progress bar code */
+    .progress__fill {
+      width: 0%;
+      height: 100%;
+      background: #FFD65C;
+      transition: all 0.2s;
+    }
+    
+    /* Progress bar code */
+    .progress__text {
+      position: absolute;
+      top: 50%;
+      right: 5px;
+      transform: translateY(-50%);
+      font: bold 16px "Quicksand", sans-serif;
+      color: #000000;
+    }
+    
+    /* Gauge and Slider for motor */
+    
+    .gauge {margin:auto; width: 100%; max-width: 250px; font-family: "Roboto", sans-serif; font-size: 32px; color: #003249;}
+    .gauge__body {width: 100%; height: 0; padding-bottom: 50%; background: #003249; position: relative; border-top-left-radius: 100% 200%;
+     border-top-right-radius: 100% 200%; overflow: hidden; }
+    .gauge__fill { position: absolute; top: 100%; left: 0; width: inherit; height: 100%; background: #9fc5e8; transform-origin: center top;
+     transform: rotate(0.25turn);
+     transition: transform 0.2s ease-out;}
+    .gauge__cover {width: 75%; height: 150%; background: #ffffff; border-radius: 50%; position: absolute; top: 25%; left: 50%; transform: translateX(-50%);
+     display: flex; align-items: center; justify-content: center; padding-bottom: 25%; box-sizing: border-box; }
+     .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #9fc5e8;
+      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
+    .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}
+    .slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; }
+    * {box-sizing: border-box}
+    
+    /* Set height of body and the document to 100% */
+    
+    body, html {
+      height: 100%;
+      margin: 0;
+      font-family: Arial;
+    }
+    /* Style tab links */
+    
+    .tablink {
+      background-color: #555;
+      color: white;
+      float: left;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      padding: 14px 16px;
+      font-size: 20px;
+      width: 33.33%;
+    }
+    .tablink:hover {
+      background-color: #777;
+    }
+    /* Style the tab content (and add height:100% for full page content) */
+    
+    .tabcontent {
+      color: black;
+      display: none;
+      padding: 10px 20px;
+    }
+    /* Button to add and delete valuues from the table */
+    .button {
+      border: none;
+      color: white;
+      padding: 10px 10px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      margin: 4px 2px;
+      transition-duration: 0.4s;
+      cursor: pointer;
+      border-radius: 8px;
+    }
+    .button1 {
+      font-size: 20px;
+      background-color:green; 
+      border: 5px solid grey;
+      color: white; 
+    }
+    .button1:hover {
+      border: 5px solid white;
+      color: white;
+    }
+    
+    .button2 {
+      font-size: 20px;
+      background-color: white; 
+      color: black; 
+      border: 8px solid #0F52BA;
+    }
+    .button2:hover {
+      background-color: #0F52BA;
+      color: white;
+    }
+    
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        text-align: left;
+        padding: 8px;
+    }
+    tr:nth-child(even){background-color: #f2f2f2}
+    th {
+      background-color: #003249;
+      color: white;
+    }
+    .center {
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <button class="tablink" onclick="openPage('explore', this, 'purple')" id="defaultOpen">Explore <i class="fa-brands fa-wpexplorer"></i></button>
+    <button class="tablink" onclick="openPage('train', this, '#0F52BA')" >Train <i class="fa-solid fa-code-branch"></i></button>
+    <button class="tablink" onclick="openPage('play', this, 'green')">Play <i class="fa-solid fa-play"></i></button>
+    
+    <div id="explore" class="tabcontent">
+        <h2> Explore Motor and Light sensor </h2>
+    </div>
+    
+    <div id="train" class="tabcontent">
+        <h2> Train Motor and Light sensor </h2>
+    </div>
+    
+    <div id="play" class="tabcontent">
+      <p> <button class="button button1" type="button" onclick="test()" id="toggle_play">Play!</button></p> 
+      
+    </div>
+    
+    <div id="motor" class="tabcontent">
+        <div class="gauge">
+          <div class="gauge__body">
+            <div class="gauge__fill"></div>
+            <div class="gauge__cover"></div>
+          </div>
+        </div>
+    </div>
+    
+    <div id="slider_thing" class="tabcontent">
+        <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="180" value="%SLIDERVALUE%" step="1" class="slider"></p>
+    </div>
+    
+    <div id="light_sensor" class="tabcontent">
+        <! –– Progress Bar code  ––>
+        <div class="progress">
+          <div class="progress__fill"></div>
+          <span class="progress__text">0%</span>
+        </div>
+        
+        <p>Light Sensor Reading <strong><span id="temp">--.-</span> </strong></p>
+    </div>
+    
+    <div id="train_test_button" class="tabcontent">
+        
+        <p>
+            <button class="button button2" type="button" onclick="addvalue()">Add Value</button>
+            <button class="button button2" type="button" onclick="deletevalue()">Delete Value</button>
+        </p>
+    </div>
+    <div id="tableeee" class="tabcontent">
+    
+        <h3>Training Values</h3>
+        <table id="myTable" class="center">
+        <thead>
+             <th> Sensor Value </th>
+             <th> Motor Value </th>
+        </thead>
+        <tbody id="tableBody">
+             
+        </tbody>
+        </table>
+    </div>
+    
+    <script>
+            const sensor = [];
+            const motor = [];
+            // Tabs 
+            function openPage(pageName,elmnt,color) {
+              var elem = document.getElementById("heading");
+              var i, tabcontent, tablinks;
+              tabcontent = document.getElementsByClassName("tabcontent");
+              for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+              }
+              tablinks = document.getElementsByClassName("tablink");
+              for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].style.backgroundColor = "";
+              }
+              document.getElementById(pageName).style.display = "block";
+              elmnt.style.backgroundColor = color;
+              
+              // displaying the motor and light sensor only when on exploration or training mode 
+              if(pageName == "train" || pageName == "explore"){
+                document.getElementById("motor").style.display = "block";
+                document.getElementById("slider_thing").style.display = "block";
+                document.getElementById("light_sensor").style.display = "block";
+              }
+              if(pageName == "explore")
+              {
+                  stop();
+              }
+              
+              // displaying the buttons when on training mode 
+              if(pageName == "train" ){
+                stop();
+                document.getElementById("train_test_button").style.display = "block";
+                document.getElementById("tableeee").style.display = "block";
+              }
+              
+              if(pageName == "play"){
+                //document.getElementById("motor").style.display = "block";
+                document.getElementById("light_sensor").style.display = "block";
+                document.getElementById("tableeee").style.display = "block";
+              
+              }
+              
+            }
+            
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
+    
+            // upDateProgressBar takes in progressBar container and updates the display of progress bar given a value 
+            function updateProgressBar(progressBar, value) {
+              progressBar.querySelector(".progress__fill").style.width = `${value}%`;
+              progressBar.querySelector(".progress__text").textContent = `${value}%`;
+            }
+            const myProgressBar = document.querySelector(".progress");
+            
+            // set the value of the gauge 
+            function setGaugeValue(gauge, value) {
+                  if (value < 0 || value > 1) {
+                    return;
+                  }
+                  gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+                    value / 2
+                  }turn)`;
+                  gauge.querySelector(".gauge__cover").textContent = `${Math.round(
+                    value * 180
+                  )} deg`;
+            }
+    
+            // updates slidervalue 
+            function updateSliderPWM(element) {
+                  var sliderValue = document.getElementById("pwmSlider").value;
+                  const gaugeElement = document.querySelector(".gauge");
+                  setGaugeValue(gaugeElement, sliderValue/180);
+                  console.log(sliderValue);
+                  var xhr = new XMLHttpRequest();
+                  xhr.open("GET", "/slider?value="+sliderValue, true);
+                  xhr.send();
+            }
+            const gaugeElement = document.querySelector(".gauge");
+            setGaugeValue(gaugeElement, 0.5);
+            
+            // update the light sensor reading 
+            var ajaxRequest = new XMLHttpRequest();  
+       
+            function ajaxLoad(ajaxURL)  
+            {  
+                ajaxRequest.open('GET',ajaxURL,true);  
+                ajaxRequest.onreadystatechange = function()  
+                {  
+                    if(ajaxRequest.readyState == 4 && ajaxRequest.status==200)  
+                     {  
+                        var ajaxResult = ajaxRequest.responseText;  
+                        var tmpArray = ajaxResult.split("|");  
+                        document.getElementById("temp").innerHTML = tmpArray[0];
+                        updateProgressBar(myProgressBar, parseInt(ajaxResult));
+                        
+                     }
+                 
+                }  
+                ajaxRequest.send();  
+            }  
+             
+            function updateDHT()   
+            {   
+                ajaxLoad('getDHT');   
+            }
+            // Controls how often the sensor is read 
+            setInterval(updateDHT, 1000);
+            
+            // add a row to the table
+            function addvalue(){
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "/?addvalue", true);
+                xhttp.send();
+                
+                // Get the table body element in which you want to add row
+                let table = document.getElementById("tableBody");
+           
+                // Create row element
+                let row = document.createElement("tr")
+          
+                // Create cells
+                let c1 = document.createElement("td")
+                let c2 = document.createElement("td")
+              
+                // get slider value reading
+                var sliderValue = document.getElementById("pwmSlider").value;
+                // get sensor reading
+                var sensorValue = document.getElementById("temp").innerHTML;
+                
+                sensor.push(sensorValue);
+                motor.push(sliderValue);
+              
+                // Insert data to cells
+                c1.innerText = sensorValue;
+                c2.innerText = sliderValue;
+          
+                // Append cells to row
+                row.appendChild(c1);
+                row.appendChild(c2);
+              
+                // Append row to table body
+                table.appendChild(row)
+            }
+            
+            function deletevalue() {
+                var xhttp = new XMLHttpRequest();
+                
+                xhttp.open("GET", "/?deletevalue", true);
+                xhttp.send();
+                
+                var table = document.getElementById('myTable');
+                var rowCount = table.rows.length;
+                table.deleteRow(rowCount -1);
+                sensor.pop();
+                motor.pop();
+            }
+            
+            function test(){
+                var button_text = document.getElementById('toggle_play').innerHTML;
+                if(button_text == "Play!"){
+                    document.getElementById('toggle_play').innerHTML = "Stop!";
+                    document.getElementById('toggle_play').style.backgroundColor = 'red';
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.open("GET", "/?test", true);
+                    xhttp.send();
+                      
+                } else {
+                    stop();
+                }
+            }
+            
+            function stop(){
+                document.getElementById('toggle_play').innerHTML = "Play!";
+                document.getElementById('toggle_play').style.backgroundColor = 'green';
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "/?stop", true);
+                xhttp.send();
+            }
+            
+    </script>
+       
+    </body>
+    </html> 
+    """
+    return webpage
+
